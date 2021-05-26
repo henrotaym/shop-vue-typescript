eab5e996f69b34557e0285ff9fc3c38d50b701dd
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-05-26 04:04:41 +0200

    💄 Product pagination empty result UI.

--- src/components/Products/Pagination.vue
+++ src/components/Products/Pagination.vue
@@ -1,10 +1,20 @@
 <template>
-  <default-pagination :per-page="5" :elements="products" v-slot="{ visible }">
-    <v-product
-      :product="product"
-      :key="`product_${product.id}`"
-      v-for="product in visible"
-    />
+  <default-pagination :per-page="5" :elements="products">
+    <template v-slot:visible="{ visible }">
+      <v-product
+        :product="product"
+        :key="`product_${product.id}`"
+        v-for="product in visible"
+      />
+    </template>
+    <template v-slot:empty>
+      <div class="flex flex-col items-center">
+        <v-icon name="announcement" class="text-5xl text-green-500 my-10" />
+        <span class="text-gray-400"
+          >There isn't any product matching search...</span
+        >
+      </div>
+    </template>
   </default-pagination>
 </template>
 
@@ -13,10 +23,11 @@ import { Component } from "vue-property-decorator";
 import Vue from "@/strong-vue";
 import DefaultPagination from "@/components/_includes/Pagination.vue";
 import VProduct from "@/components/Products/Product.vue";
+import VIcon from "@/components/_includes/Icon.vue";
 import { GetterTypes } from "@/store/types";
 
 @Component({
-  components: { DefaultPagination, VProduct },
+  components: { DefaultPagination, VProduct, VIcon },
 })
 export default class ProductPagination extends Vue {
   private get products() {
